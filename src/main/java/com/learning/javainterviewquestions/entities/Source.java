9b01eef9bafa059bb79a87f57883746269e30b70
Ernesto Orozco <96429978+gmdeorozco@users.noreply.github.com>
2023-01-13 10:34:16 +0000

    ll

--- src/main/java/com/learning/javainterviewquestions/entities/Source.java
+++ src/main/java/com/learning/javainterviewquestions/entities/Source.java
@@ -1,32 +1,4 @@
-package com.learning.javainterviewquestions.entities;
 
-import java.util.List;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Data @AllArgsConstructor @NoArgsConstructor @Builder
-public class Source {
-    @Id
-    private Long id;
-
-    private double elo;
-
-    @OneToMany( mappedBy = "source")
-    private List<QuestionEntity> questions;
-
-    private String sourceLink;
-    private String topic;
-
-
-}
 package com.learning.javainterviewquestions.entities;
 
 import java.util.ArrayList;
@@ -53,4 +25,6 @@ public class Source{
 
     @OneToMany(mappedBy="source")
     List<QuestionEntity> questions =  new ArrayList<>();
+
+    private String topic;
 }
