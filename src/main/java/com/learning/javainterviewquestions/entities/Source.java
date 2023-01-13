41cc2301bf2cd3bf8ad8c56de2c7a63a90447a59
Ernesto Orozco <96429978+gmdeorozco@users.noreply.github.com>
2023-01-13 10:54:05 +0000

    yes

--- src/main/java/com/learning/javainterviewquestions/entities/Source.java
+++ src/main/java/com/learning/javainterviewquestions/entities/Source.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
@@ -14,7 +16,7 @@ import lombok.Data;
 @Entity @NoArgsConstructor @AllArgsConstructor @Data
 public class Source{
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private double elo;
