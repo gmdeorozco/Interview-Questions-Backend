3567e6608020f106080f2551309f8b2616de1c00
gmdeorozco <ernesto.orozco.coulson@gmail.com>
2023-01-29 07:31:35 +0000

    added elo feature

--- src/main/java/com/learning/javainterviewquestions/elo/EloResult.java
+++ src/main/java/com/learning/javainterviewquestions/elo/EloResult.java
@@ -1,9 +1,11 @@
 package com.learning.javainterviewquestions.elo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Builder @Data
+@Builder @Data @NoArgsConstructor @AllArgsConstructor
 public class EloResult {
     private Player player1;
     private Player player2;
--- src/main/java/com/learning/javainterviewquestions/elo/Player.java
+++ src/main/java/com/learning/javainterviewquestions/elo/Player.java
@@ -1,5 +1,11 @@
 package com.learning.javainterviewquestions.elo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder @Data @NoArgsConstructor @AllArgsConstructor
 public class Player {
     String oldElo;
     String newElo;
